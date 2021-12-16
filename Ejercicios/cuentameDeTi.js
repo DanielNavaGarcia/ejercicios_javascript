cc455797ff09c87c4790d32761aa9dc22dbb0160
Daniel Nava <danielnavagarcia1@gmail.com>
2021-12-16 04:42:59 -0600

    commit

---
+++ Ejercicios/cuentameDeTi.js
@@ -0,0 +1,7 @@
+let nombre = "Daniel Nava Garcia";
+let numeroFavorito = 97;
+let tienesMascotas = false;
+
+console.log("Mi nombre es: " + nombre + " que es de tipo " + typeof nombre);
+console.log("Mi numero favorito es: " + numeroFavorito + " que es de tipo " + typeof numeroFavorito);
+console.log("Mi respuesta a que si yo tengo mascotas: " + tienesMascotas + " que es de tipo " + typeof tienesMascotas);
---
+++ Ejercicios/tiposDeDatos.js
@@ -0,0 +1,40 @@
+let saludo = "hola mundo"; //String
+console.log("el tipo de dato es " + typeof saludo)
+
+let edad = 24;  //numero
+console.log("el tipo de dato es " + typeof edad)
+
+
+let nombre = "luis";    //String
+console.log("el tipo de dato es " + typeof salunombredo)
+
+
+let mayor18 = true;     // Boolean
+console.log("el tipo de dato es " + typeof mayor18)
+
+let x;  //Undefiend
+console.log("el tipo de dato es " + typeof x)
+
+let bateria = "25";     //String
+console.log("el tipo de dato es " + typeof bateria)
+
+let tienesMascotas = "false";   //String
+console.log("el tipo de dato es " + typeof tienesMascotas)
+
+let promedio = 238.90;  // numero
+console.log("el tipo de dato es " + typeof promedio)
+
+let nada = null;    //nulo
+console.log("el tipo de dato es " + typeof nada)
+
+let y;  //Undefiend
+console.log("el tipo de dato es " + typeof y)
+
+
+/* typeof 
+dice el tipo de dato */
+
+/* 
+manera 1 typeof(variable)
+manera 2 typeof variable
+*/
